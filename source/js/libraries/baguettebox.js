9fd27559dcf45a97156e5551d10d4119066bd948
Fernando Javier Averanga Aruquipa <nandes.ingsistemas@gmail.com>
2020-10-30 00:46:58 -0400

    UP: Version 1.0 Data full visulitation

--- source/js/libraries/baguettebox.js
+++
@@ -1,8 +0,0 @@
-/*
-  |--------------------------------------------------------------------------
-  | baguetteBox.js - Simple and easy to use lightbox script written in pure JavaScript
-  |--------------------------------------------------------------------------
-  |
-  | npm install baguettebox.js --save
-  |
-  */

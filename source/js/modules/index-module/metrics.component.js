ad4a8c69c161b89f28502115d3677c6a8d290799
Fernando Javier Averanga Aruquipa <nandes.ingsistemas@gmail.com>
2020-10-29 21:09:11 -0400

    IMP: metrics completed full dashbord data

--- source/js/modules/index-module/metrics.component.js
+++ source/js/modules/index-module/metrics.component.js
@@ -14,21 +14,248 @@ export default window.metricsComponent = function () {
     borderColor: null,
     title: 'Cantidad de post en Facebook',
     titleColor: null,
+    images: [],
+    getDataPosts (socialData) {
+      this.labels = socialData.map((v, k) => { return v.user })
+      this.dataChart = socialData.map((v, k) => { return v.posts })
+    },
     chartMetricsInit(chartId, socialMedia) {
       switch (socialMedia) {
         case 'facebook':
-          this.labels = Facebook.map((v, k) => { return v.user })
-          this.dataChart = Facebook.map((v, k) => { return v.posts })
-          console.log(Facebook.map((v, k) => { return v.posts }))
+          this.label = 'Cantidad de posts en Facebook'
+          this.images = [
+            {
+              src: `${this.resourcePath}/luis-arce.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/carlos-mesa.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/luis-f-camacho.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/chi-hyung-chung.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/feliciano-mamani.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/maria-cruz-baya.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/jorge-quiroga.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/mas-ipsp.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/comunidad-ciudadana.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/creemos.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/fpv.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/pan-bol.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/libre21.jpg`,
+              width: 20,
+              height: 20
+            }
+          ]
+          this.backgroundColor = [
+            '#2b6cb0', // luis-arce
+            '#ed8936', // carlos-mesa
+            '#F0005F', // creemos
+            '#006B46', // fpv
+            '#FF0000', // panbol
+            '#000000', // adn
+            '#009D00', // libre21
+            '#2b6cb0', // luis-arce
+            '#ed8936', // carlos-mesa
+            '#F0005F', // creemos
+            '#006B46', // fpv
+            '#FF0000', // panbol
+            '#009D00' // libre21
+          ]
+          this.borderColor = [
+            '#000000', // luis-arce
+            '#38a169', // carlos-mesa
+            '#167E7D', // creemos
+            '#F5E900', // fpv
+            '#807F93', // panbol
+            '#E3000E', // adn
+            '#E3000E', // libre21
+            '#000000', // luis-arce
+            '#38a169', // carlos-mesa
+            '#167E7D', // creemos
+            '#F5E900', // fpv
+            '#807F93', // panbol
+            '#E3000E' // libre21
+          ]
+          this.getDataPosts(Facebook)
           break;
         case 'instagram':
-          console.log(Instagram)
+          this.label = 'Cantidad de posts en Instagram'
+          this.images = [
+            {
+              src: `${this.resourcePath}/luis-arce.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/carlos-mesa.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/luis-f-camacho.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/chi-hyung-chung.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/jorge-quiroga.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/mas-ipsp.jpg`,
+              width: 20,
+              height: 20
+            }
+          ]
+          this.backgroundColor = [
+            '#2b6cb0', // luis-arce
+            '#ed8936', // carlos-mesa
+            '#F0005F', // creemos
+            '#006B46', // fpv
+            '#009D00', // libre21
+            '#2b6cb0', // luis-arce
+          ]
+          this.borderColor = [
+            '#000000', // luis-arce
+            '#38a169', // carlos-mesa
+            '#167E7D', // creemos
+            '#F5E900', // fpv
+            '#E3000E', // libre21
+            '#000000', // luis-arce
+          ]
+          this.getDataPosts(Instagram)
           break;
         case 'twitter':
-          console.log(Twitter)
+          this.label = 'Cantidad de posts en Twitter'
+          this.images = [
+            {
+              src: `${this.resourcePath}/luis-arce.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/carlos-mesa.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/luis-f-camacho.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/chi-hyung-chung.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/jorge-quiroga.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/mas-ipsp.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/comunidad-ciudadana.jpg`,
+              width: 20,
+              height: 20
+            }
+          ]
+          this.backgroundColor = [
+            '#2b6cb0', // luis-arce
+            '#ed8936', // carlos-mesa
+            '#F0005F', // creemos
+            '#006B46', // fpv
+            '#009D00', // libre21
+            '#2b6cb0', // luis-arce
+            '#ed8936', // carlos-mesa
+          ]
+          this.borderColor = [
+            '#000000', // luis-arce
+            '#38a169', // carlos-mesa
+            '#167E7D', // creemos
+            '#F5E900', // fpv
+            '#E3000E', // libre21
+            '#000000', // luis-arce
+            '#38a169', // carlos-mesa
+          ]
+          this.getDataPosts(Twitter)
           break
         case 'youtube':
-          console.log(Youtube)
+          this.label = 'Cantidad de transcripciones en Youtube'
+          this.images = [
+            {
+              src: `${this.resourcePath}/jorge-quiroga.jpg`,
+              width: 20,
+              height: 20
+            },
+            {
+              src: `${this.resourcePath}/carlos-mesa.jpg`,
+              width: 20,
+              height: 20
+            }
+          ]
+          this.backgroundColor = [
+            '#009D00', // libre21
+            '#ed8936' // carlos-mesa
+          ]
+          this.borderColor = [
+            '#E3000E', // libre21
+            '#38a169' // carlos-mesa
+          ]
+          this.getDataPosts(Youtube)
           break
         default:
           break
@@ -39,27 +266,11 @@ export default window.metricsComponent = function () {
           labels: this.labels,
           datasets: [
             {
-						  label: 'Cantidad de publicaciones en Facebook',
+						  label: this.label,
 						  data: this.dataChart,
-						  backgroundColor: [
-						    '#2b6cb0',
-						    '#ed8936',
-						    '#F0005F',
-						    '#006B46',
-						    '#FF0000',
-								'#000000',
-								'#009D00'
-						  ],
-						  fill: false,
-						  borderColor: [
-			          '#000000',
-			          '#38a169',
-			          '#167E7D',
-			          '#F5E900',
-			          '#807F93',
-								'#E3000E',
-								'#E3000E'
-						  ],
+              fill: false,
+						  backgroundColor: this.backgroundColor,
+						  borderColor: this.borderColor,
 						  borderWidth: 2,
 						  hoverOffset: 4
             }           
@@ -67,7 +278,7 @@ export default window.metricsComponent = function () {
         },
         options: {          
           title: {
-            display: true,
+            display: false,
             text: this.title
             // fontColor: this.titleColor
           },
@@ -86,48 +297,12 @@ export default window.metricsComponent = function () {
             labels: {
               render: 'image',
               textMargin: 10,
-              images: [
-								{
-                  src: `${this.resourcePath}/luis-arce.jpg`,
-                  width: 40,
-                  height: 40
-                },
-								{
-                  src: `${this.resourcePath}/carlos-mesa.jpg`,
-                  width: 40,
-                  height: 40
-								},
-								{
-                  src: `${this.resourcePath}/luis-camacho.jpg`,
-                  width: 40,
-                  height: 40
-								},
-								{
-                  src: `${this.resourcePath}/chi-hyung-chung.jpg`,
-                  width: 40,
-                  height: 40
-								},
-								{
-                  src: `${this.resourcePath}/feliciano-mamani.jpg`,
-                  width: 40,
-									height: 40
-								},
-								{
-                  src: `${this.resourcePath}/maria-cruz.jpg`,
-                  width: 40,
-                  height: 40
-								},
-								{
-                  src: `${this.resourcePath}/jorge-quiroga.jpg`,
-                  width: 40,
-                  height: 40
-                }
-              ]
+              images: this.images
             }
 					},
 					layout: {
 						padding: {
-							top: 10
+							top: 30
 						}
 					},
           responsive: true,

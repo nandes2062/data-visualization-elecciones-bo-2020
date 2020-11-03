4fa2b975c5960fc8172388eadd451b9d6598cd47
nandes <nandes.ingsistemas@gmail.com>
2020-11-02 20:55:58 -0400

    UP: update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,28 +14,10 @@ module.exports = {
     },
     fontFamily: {
       sans: [
-        'Nunito Sans',
-        'Roboto',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"'
+        'Karla', 'Open Sans', 'sans-serif'
       ],
-      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-      mono: [
-        'Menlo',
-        'Monaco',
-        'Consolas',
-        '"Liberation Mono"',
-        '"Courier New"',
-        'monospace'
+      serif: [
+        'Rubik', 'Arial', 'sans-serif'
       ]
     }
   },
